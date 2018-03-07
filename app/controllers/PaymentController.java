475e9d7678def9d3ce5513bbcac86df47af266e2
xinpan <xpan@thoughtworks.com>
2018-03-07 14:58:33 +0800

    ESK-5 init payment API

---
+++ app/controllers/PaymentController.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class PaymentController extends Controller {
+    public Result apply() {
+        return ok();
+    }
+}
